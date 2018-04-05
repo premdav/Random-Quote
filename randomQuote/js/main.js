573dc6f547c7297915f8130c44e37685eeabe2f4
premdav <prem.dav@outlook.com>
2018-04-05 10:10:43 -0500

    Random quote API connected and working. Minor styling changes.

--- randomQuote/js/main.js
+++ randomQuote/js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
-    $(".btnQuote").click(function(){
-        $(".insertQuote").html("This is a test quote. Does it work?");
+    $(".btnQuote").click(function () {
+        // Calling the random quote API and displaying it in the box
+        $.ajax({
+           url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+            dataType: 'json',
+            success: function (data) {
+               var post = data.shift(); // data is in array of posts. this grabs the first one
+                $(".insertQuote").html(post.content);
+                $(".insertAuthor").html(post.title);
+            },
+            cache: false
+        });
     });
 });
