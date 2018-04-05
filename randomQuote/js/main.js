08787338fc0a3ea8a49dd2a48e7a95dbb14642c9
premdav <prem.dav@outlook.com>
2018-04-05 10:43:38 -0500

    Random quote loads up immediately at start. Background color changes with click.

--- randomQuote/js/main.js
+++ randomQuote/js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
+    // setting global variables
+    let clickCount = 0;
+    // Loading random quote in as initial text
+    $.ajax({
+        url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+        dataType: 'json',
+        success: function (data) {
+            var post = data.shift(); // data is in array of posts. this grabs the first one
+            $(".insertQuote").html(post.content);
+            $(".insertAuthor").html(post.title);
+        },
+        cache: false
+    });
+
     $(".btnQuote").click(function () {
         // Calling the random quote API and displaying it in the box
         $.ajax({
@@ -11,5 +25,13 @@ $(document).ready(function () {
             },
             cache: false
         });
+
+        // Changing to random background color on click
+        let myColors = ['42f4ce', '8ca5ce', 'de93e2', 'e293a5', 'e2b193', 'dbe293', '93e296' ];
+        $('body').css("background-color", "#"+myColors[clickCount]);
+        clickCount += 1;
+        if(clickCount === myColors.length) {
+            clickCount = 0;
+        }
     });
 });
